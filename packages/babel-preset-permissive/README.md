349a0ef6d0c95c13467971e0526077e181761edd
Nikolay Stoynov <arvigeus@gmail.com>
2020-03-29 13:24:31 +0300

    feat(babel-preset-permissive): add config

---
+++ packages/babel-preset-permissive/README.md
@@ -0,0 +1,38 @@
+# babel-preset-permissive
+
+## Install
+
+```shell
+npm install --dev babel-preset-permissive
+```
+
+```json
+// .babelrc
+{
+  "presets": ["permissive"]
+}
+```
+
+## What's inside
+
+### Presets
+
+- [`@babel/preset-env`](https://babeljs.io/docs/en/babel-preset-env)
+- [`@babel/preset-react`](https://babeljs.io/docs/en/babel-preset-react)
+- [`@babel/preset-typescript`](https://babeljs.io/docs/en/babel-preset-typescript)
+
+### Plugins
+
+#### Proposal
+
+- [`@babel/plugin-proposal-decorators`](https://babeljs.io/docs/en/babel-plugin-proposal-decorators)
+- [`@babel/plugin-proposal-class-properties`](https://babeljs.io/docs/en/babel-plugin-proposal-class-properties)
+- [`@babel/plugin-proposal-numeric-separator`](https://babeljs.io/docs/en/babel-plugin-proposal-numeric-separator)
+- [`@babel/plugin-syntax-dynamic-import`](https://babeljs.io/docs/en/babel-plugin-syntax-dynamic-import)
+- [`@babel/plugin-transform-runtime`](https://babeljs.io/docs/en/babel-plugin-transform-runtime)
+- [`@babel/plugin-transform-react-jsx`](https://babeljs.io/docs/en/babel-plugin-transform-react-jsx)
+- [`babel-plugin-annotate-pure-calls`](https://github.com/Andarist/babel-plugin-annotate-pure-calls)
+- [`babel-plugin-dev-expression`](https://github.com/4Catalyzer/babel-plugin-dev-expression)
+- [`babel-plugin-macros`](https://github.com/kentcdodds/babel-plugin-macros)
+- [`babel-plugin-preval`](https://github.com/kentcdodds/babel-plugin-preval)
+- [babel-plugin-react-remove-properties](https://github.com/oliviertassinari/babel-plugin-react-remove-properties)
---
+++ packages/babel-preset-permissive/src/babel.config.js
@@ -0,0 +1,67 @@
+module.exports = (api, _options) => ({
+  presets: [
+    [
+      require("@babel/preset-env").default,
+      api.env("test")
+        ? { targets: { node: "current", shippedProposals: true } }
+        : {
+            useBuiltIns: "entry",
+            shippedProposals: true,
+            corejs: 3,
+            modules: false,
+            exclude: ["transform-typeof-symbol"],
+          },
+    ],
+    [
+      require("@babel/preset-react").default,
+      { development: !api.env("production"), useBuiltIns: true },
+    ],
+    require("@babel/preset-typescript").default,
+  ],
+  plugins: [
+    [
+      require("@babel/plugin-transform-runtime").default,
+      {
+        version: require("@babel/runtime/package.json").version,
+        useESModules: true,
+      },
+    ],
+    require("babel-plugin-annotate-pure-calls").default,
+    require("babel-plugin-dev-expression"),
+    require(`@babel/plugin-syntax-dynamic-import`).default,
+    api.env("test") && require(`babel-plugin-dynamic-import-node`).default,
+
+    // NOTE: This goes before class properties
+    // https://babeljs.io/docs/en/babel-plugin-proposal-decorators#note-compatibility-with-babel-plugin-proposal-class-properties
+    [require("@babel/plugin-proposal-decorators").default, false],
+    [
+      require("@babel/plugin-proposal-class-properties").default,
+      { loose: true },
+    ],
+    require("@babel/plugin-proposal-numeric-separator").default,
+    // Optional chaining and nullish coalescing are supported in @babel/preset-env,
+    // but not yet supported in webpack due to support missing from acorn.
+    // These can be removed once webpack has support.
+    // See https://github.com/facebook/create-react-app/issues/8445#issuecomment-588512250
+    require("@babel/plugin-proposal-optional-chaining").default,
+    require("@babel/plugin-proposal-nullish-coalescing-operator").default,
+
+    require("babel-plugin-macros"),
+    require("babel-plugin-preval"),
+
+    require("@babel/plugin-transform-react-jsx").default,
+    api.env("production") &&
+      require("babel-plugin-react-remove-properties").default,
+  ].filter(Boolean),
+  overrides: [
+    {
+      test: /\.tsx?$/,
+      plugins: [
+        [
+          require("@babel/plugin-proposal-decorators").default,
+          { legacy: true },
+        ],
+      ],
+    },
+  ],
+});
