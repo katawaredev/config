9dec91c839e4ad599bf399e88d6c9787fd6ab7ce
Nikolay Stoynov <arvigeus@gmail.com>
2020-05-06 06:32:14 +0300

    fix(typescript-config-permissive): change dev definition to const

--- packages/typescript-config-permissive/types/index.d.ts
+++ packages/typescript-config-permissive/types/index.d.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+declare const __DEV__: boolean;
+
 declare module "*.module.css" {
   const classes: { readonly [key: string]: string };
   export default classes;
